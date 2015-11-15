217a309158041757706c1658fdb69db1f496d4b5
Abass Sesay <bassco10@gmail.com>
2015-11-14 21:51:11 -0500

    SIGABRT error

--- BookStore3/ViewController.swift
+++ BookStore3/ViewController.swift
@@ -7,12 +7,57 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var managedObjectContext: NSManagedObjectContext!;
+    
+    func loadBooks() -> Array<AnyObject> {
+        var error: NSError? = nil
+        var fetchRequest = NSFetchRequest(entityName: "Book")
+        NSFetchRequest(entityName: "Book")
+        var result = [AnyObject]()
+        do {
+            result = try
+            managedObjectContext!.executeFetchRequest(fetchRequest)
+        } catch let error as NSError {
+            NSLog("My Error: %@", error)
+        };
+        return result
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let appDelegate: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        managedObjectContext = appDelegate.managedObjectContext as NSManagedObjectContext
+        
+    }
+//    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+//            return 1
+//    }
+    
+    
+    func tableView(tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int {
+                return 1
+    }
+    
+//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        var cell = tableView.dequeueReusableCellWithIdentifier("Cell") as? UITableViewCell
+//        
+//        var book: Book = loadBooks()[indexPath.row] as! Book
+//        cell.textLabel.text = book.title
+//    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            
+            let cell = tableView.dequeueReusableCellWithIdentifier("Cell")
+            var book: Book = loadBooks()[indexPath.row] as! Book
+            cell!.textLabel!.text = book.title
+            
+            return cell!
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +66,19 @@ class ViewController: UIViewController {
     }
 
     @IBAction func addNew(sender: AnyObject) {
+        let entity = NSEntityDescription.entityForName("Book", inManagedObjectContext:managedObjectContext)
+        
+        var book = Book(entity: entity!,insertIntoManagedObjectContext:managedObjectContext)
+        
+        book.title = "My Book:" + String(loadBooks().count)
+        
+        var error: NSError?
+//        managedObjectContext.save(&error)
+//         myTableView.reloadData()
+//        self.tableView.reloadData()
+        
     }
-
 }
+
+
 
